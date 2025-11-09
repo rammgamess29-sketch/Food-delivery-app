132575a5dac431c365ebb871b70a979a3cdc58aa
rammgamess29-sketch <rammgamess29@gmail.com>
2025-11-09 12:24:41 +0530

    Add files via upload

---
+++ server/middelware/rateLimiter.js
@@ -0,0 +1,13 @@
+const rateLimit = require('express-rate-limit');
+
+exports.apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.'
+});
+
+exports.authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: 'Too many authentication attempts, please try again later.'
+});
